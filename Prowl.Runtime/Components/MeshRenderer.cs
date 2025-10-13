8baca43cc879d077965cf8fe79f533d73174e529
Wulferis <acer1171090@gmail.com>
2025-10-14 03:06:23 +1100

    Refactor MeshRenderer to implement IRenderable
    MeshRenderer now implements the IRenderable interface, providing methods for rendering and culling data. The rendering logic has been updated to push the MeshRenderer itself as a renderable, and property state management has been improved for efficiency.

--- Prowl.Runtime/Components/MeshRenderer.cs
+++ Prowl.Runtime/Components/MeshRenderer.cs
@@ -4,28 +4,43 @@
 using Prowl.Runtime.Rendering;
 using Prowl.Runtime.Resources;
 using Prowl.Vector;
+using Prowl.Vector.Geometry;
 
 namespace Prowl.Runtime;
 
-public class MeshRenderer : MonoBehaviour
+public class MeshRenderer : MonoBehaviour, IRenderable
 {
     public Mesh Mesh;
     public Material Material;
     public Color MainColor = Color.White;
 
+    private PropertyState _properties = new PropertyState();
+
     public override void Update()
     {
         if (Mesh != null && Material != null)
         {
-            PropertyState properties = new PropertyState();
-            properties.SetInt("_ObjectID", InstanceID);
-            properties.SetColor("_MainColor", MainColor);
-            GameObject.Scene.PushRenderable(new MeshRenderable(
-                Mesh,
-                Material,
-                Transform.localToWorldMatrix,
-                this.GameObject.layerIndex,
-                properties));
+            _properties.Clear();
+            _properties.SetInt("_ObjectID", InstanceID);
+            _properties.SetColor("_MainColor", MainColor);
+            GameObject.Scene.PushRenderable(this);
         }
+    }
+
+    public Material GetMaterial() => Material;
+    public int GetLayer() => this.GameObject.layerIndex;
+
+    public void GetRenderingData(ViewerData viewer, out PropertyState properties, out Mesh drawData, out Double4x4 model)
+    {
+        drawData = Mesh;
+        properties = _properties;
+        model = Transform.localToWorldMatrix;
+    }
+
+    public void GetCullingData(out bool isRenderable, out AABB bounds)
+    {
+        isRenderable = true;
+        //bounds = Bounds.CreateFromMinMax(new Vector3(999999), new Vector3(999999));
+        bounds = Mesh.bounds.TransformBy(Transform.localToWorldMatrix);
     }
 }
