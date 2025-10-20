04fc53f6b007050c25986c8793de83c5359fc5d6
Abdiel Lopez <48071553+PaperPrototype@users.noreply.github.com>
2025-10-20 11:13:26 -0400

    Movement now works with controllers, TODO for move controller support

--- Samples/Playground/Components/LookCamera.cs
+++ Samples/Playground/Components/LookCamera.cs
@@ -21,14 +21,54 @@ public class LookCamera : MonoBehaviour
         }
     }
 
+    // Input Actions
+    private InputActionMap inputMap = null!;
+    private InputAction moveAction = null!;
+    private InputAction lookAction = null!;
+    private InputAction lookEnableAction = null!;
+    private InputAction flyUpAction = null!;
+    private InputAction flyDownAction = null!;
+    private InputAction sprintAction = null!;
+
+    public override void Start()
+    {
+        inputMap = new InputActionMap("Playground Game");
+
+        // Movement (WASD + Gamepad)
+        moveAction = inputMap.AddAction("Move", InputActionType.Value);
+        moveAction.ExpectedValueType = typeof(Double2);
+
+        // WASD
+        moveAction.AddBinding(new Vector2CompositeBinding(
+            InputBinding.CreateKeyBinding(KeyCode.S),
+            InputBinding.CreateKeyBinding(KeyCode.W),
+            InputBinding.CreateKeyBinding(KeyCode.A),
+            InputBinding.CreateKeyBinding(KeyCode.D),
+            true
+        ));
+
+        // JOYSTICK
+        var leftStick = InputBinding.CreateGamepadAxisBinding(0, deviceIndex: 0);
+        leftStick.Processors.Add(new DeadzoneProcessor(0.15f));
+        leftStick.Processors.Add(new NormalizeProcessor());
+        moveAction.AddBinding(leftStick);
+
+        Input.RegisterActionMap(inputMap);
+        inputMap.Enable();
+    }
+
     public override void Update()
     {
+        Double2 axis = moveAction.ReadValue<Double2>();
+
         // Camera movement
         Double3 movement = Double3.Zero;
-        if (Input.GetKey(KeyCode.W)) movement += Transform.Forward * Time.DeltaTime * METERS_PER_SECOND;
-        if (Input.GetKey(KeyCode.S)) movement -= Transform.Forward * Time.DeltaTime * METERS_PER_SECOND;
-        if (Input.GetKey(KeyCode.D)) movement += Transform.Right * Time.DeltaTime * METERS_PER_SECOND;
-        if (Input.GetKey(KeyCode.A)) movement -= Transform.Right * Time.DeltaTime * METERS_PER_SECOND;
+        // if (Input.GetKey(KeyCode.W)) movement += Transform.Forward * Time.DeltaTime * METERS_PER_SECOND;
+        // if (Input.GetKey(KeyCode.S)) movement -= Transform.Forward * Time.DeltaTime * METERS_PER_SECOND;
+        // if (Input.GetKey(KeyCode.D)) movement += Transform.Right * Time.DeltaTime * METERS_PER_SECOND;
+        // if (Input.GetKey(KeyCode.A)) movement -= Transform.Right * Time.DeltaTime * METERS_PER_SECOND;
+        movement += Transform.Forward * -axis.Y * Time.DeltaTime * METERS_PER_SECOND;
+        movement += Transform.Right * axis.X * Time.DeltaTime * METERS_PER_SECOND;
         if (Input.GetKey(KeyCode.E)) movement += Transform.Up * Time.DeltaTime * METERS_PER_SECOND;
         if (Input.GetKey(KeyCode.Q)) movement -= Transform.Up * Time.DeltaTime * METERS_PER_SECOND;
 
